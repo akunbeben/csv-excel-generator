4e74df4bd5225e76a3a9704d092deaba7a518f5c
Benny Rahmat <benny.rahmat@thrive.co.id>
2024-07-22 00:29:04 +0800

    update icon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,8 @@
 
         <title>Laravel</title>
 
+        <link rel="icon" href="{{ asset('logo.ico') }}" type="image/x-icon" />
+        
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
