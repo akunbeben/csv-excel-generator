79a7cb8b261d2e9cd8194ec5b9d3c99ad7f6a82b
Benny Rahmat <benny.rahmat@thrive.co.id>
2024-07-21 22:10:18 +0800

    disable csrf verification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Middleware\SkipVerifyCsrfToken;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append([SkipVerifyCsrfToken::class]);
+        $middleware->remove(VerifyCsrfToken::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
