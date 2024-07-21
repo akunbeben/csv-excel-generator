82f698dd2af2fbaa9c8b858830d99409df0bf99d
Benny Rahmat <benny.rahmat@thrive.co.id>
2024-07-21 14:10:13 +0800

    remap db path

--- config/database.php
+++ config/database.php
@@ -34,7 +34,7 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', '/tmp/database.sqlite'),
+            'database' => env('DB_DATABASE', public_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
