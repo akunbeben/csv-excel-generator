7f56404b26ddbf162b8be4620ea559fea02e1238
Benny Rahmat <benny.rahmat@thrive.co.id>
2024-07-21 15:32:37 +0800

    using supabase

--- config/database.php
+++ config/database.php
@@ -84,7 +84,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
